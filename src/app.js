9717698091692a3e7db275a5e96f888512a26c6c
MJ-Curtido <manu17pc@gmail.com>
2023-01-23 11:12:59 +0100

    Intento

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+let bbddChuli;
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     }
 
-    const db = client.db(databaseName)
+    bbddChuli = client.db(databaseName)
     
-    db.collection('users').insertOne({
+    bbddChuli.collection('users').insertOne({
         name: 'Andrew',
         age: 27
     })
 })
-
+app.get('',(req,res)=>{
+    res.send('ers gay tio');
+})
 app.listen(3000, () => {
     console.log("Server is up on port 3000.");
-  });
+});
