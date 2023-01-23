06266612ec6e070b98cdbffd469728850b8d0bb0
MJ-Curtido <manu17pc@gmail.com>
2023-01-23 09:19:08 +0100

    Intento

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+    
+    db.collection('users').insertOne({
+        name: 'Andrew',
+        age: 27
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000.");
+  });
