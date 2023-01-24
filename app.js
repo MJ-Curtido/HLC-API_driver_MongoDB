d88ad751297f8a6ad316cb6932c7494ab908953b
Manu <manu17pc@gmail.com>
2023-01-25 00:00:27 +0100

    Update and delete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ MongoClient.connect(
   { useNewUrlParser: true },
   (error, client) => {
     if (error) {
-      return console.log("Unable to connect to database!");
+      return console.log("Unable to connect to database.");
     }
 
     db = client.db(databaseName);
@@ -25,22 +25,51 @@ app.get("", (req, res) => {
 });
 
 app.post("/crearcontacto", (req, res) => {
-    const { nif, name, age, tel } = req.body;
+  const { nif, name, age, tel } = req.body;
 
-    if (nif == null || name == null || age == null || tel == null) return res.send({error: 'Some property is null.'});
+  if (nif == null || name == null || age == null || tel == null)
+    return res.send({ error: "Some property is null." });
 
-    db.collection('contact').insertOne({
-        nif: nif,
-        name: name,
-        age: age,
-        tel: tel
-    }, (error, result) => {
-        if (error) {
-            return res.send({error: 'Error to insert.'})
-        }
-        res.send({id: result.insertedId});
-    })
+  db.collection("contact").insertOne(
+    {
+      nif: nif,
+      name: name,
+      age: age,
+      tel: tel,
+    },
+    (error, result) => {
+      if (error) {
+        return res.send({ error: "Error to insert." });
+      }
+      res.send({ id: result.insertedId });
+    }
+  );
 });
+
+app.get("/buscarcontacto", (req, res) => {
+  const id = req.query.id;
+
+  if (id == null) {
+    return res.send({ error: "The id is null." });
+  }
+
+  db.collection("contact").findOne(
+    { _id: new ObjectId(id) },
+    (error, result) => {
+      if (error) {
+        return res.send({ error: "Unable to get the contact." });
+      }
+
+      if (result == null) {
+        return res.send({ error: "The contact with the introduced id isn't exist." });
+      }
+
+      return res.send(result);
+    }
+  );
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
